f9e59aacf8aa4e3620862a17450edc67765d9314
Vahid Karimi <dihavgaga@gmail.com>
2023-04-02 21:23:41 +0100

    fixed issue defult number

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const generatedPassword = document.getElementById("generated-password");
 const changePasswordLength = document.querySelector('.change-password-length');
 
 
-function getpass(num=12) 
+function getpass(num=6) 
 // problem is here to send the numbers of password to the function and I've set it manully 12 ...
 {
     let chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
@@ -18,15 +18,23 @@ function getpass(num=12)
   }
   
   generateBtn.addEventListener("click", function() {
+  //  debugger; 
     
-    let num; 
-    changePasswordLength.value = num;
+    let num = changePasswordLength.value;
     changePasswordLength.addEventListener('input' , () =>  num = changePasswordLength.value);
-
-    let pass = getpass(num);
+    let pass;
+    if(num !== ""){
+      pass = getpass(num);
+    }
+    else {
+      pass = getpass();
+      
+    }
     // let pass2 = getpass(num);
     
     generatedPassword.textContent = pass;
+    generatedPassword.style.background = "#10B981";   
+
     // generatedPassword2.textContent = pass2;
 
   });
