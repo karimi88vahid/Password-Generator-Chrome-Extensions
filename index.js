0dfd736b787d09c08598fd94fb9cbb01c0258a6e
Vahid Karimi <dihavgaga@gmail.com>
2023-04-03 16:01:19 +0100

    ready for use

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const changePasswordLength = document.querySelector('.change-password-length');
 function getpass(num=6) 
 // problem is here to send the numbers of password to the function and I've set it manully 12 ...
 {
-    let chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+    let chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "&", "*", "_", "-", "+", "=", ":", ";", "<", ">", ".", "?", "/"];
     
     let password = "";
     for (let i = 0; i < num; i++) {
